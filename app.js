f8bcbcdbe22ebbf141ba0f031395820b8b3d0d7d
naor <naorpio15@gmail.com>
2018-03-06 09:16:26 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+ 
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
@@ -12,20 +12,90 @@ server.listen(4000);
 console.log('connect server');
 
 var LIST_SOCKET = {};
-var LIST_PLAYER = {};
+
+var Entity = function(){
+ var self = {             x: 200,
+             y: 200,
+             speedX: 0,
+             speedY: 0,
+             id: ""
+}
+  self.update = function (){
+      self.updatePosition(); 
+  }
+  self.updatePosition = function(){
+     self.x  += self.speedX;
+     self.y  += self.speedY;
+  }
+  return self;
+}
 var Player = function(id){	
-var self = { 
-          x: 200,
-		  y: 200,
-		  id: id,
-	      number:  + Math.floor(10 * Math.random()),
-		  right:false,
-          left: false,
-          up:   false,
-          down: false,
-          speed: 10		  
-	}  
+var self = Entity();                    
+         self.id = id; 
+         self.number =    Math.floor(10 * Math.random());
+         self.right = false;
+         self.left =  false;
+         self.up =   false;
+         self.down = false;
+         self.Maxspeed = 10;		  
+    
+               var super_update = self.update;
+               self.update = function(){
+                             self.updateSpeed();
+                             super_update();
+                   }
+              
+             self.updateSpeed = function(){
+              
+                     if(self.right)
+                           self.speedX = self.Maxspeed;
+              else if(self.left)
+                           self.speedX = -self.Maxspeed;
+              else self.speedX = 0;
+
+                     if(self.up)
+                           self.speedY = -self.Maxspeed;
+              else if(self.down)
+                           self.speedY =  self.Maxspeed;
+              else self.speedY = 0;
+
+
+     }
+            Player.list[id] = self;
 	return self
+}
+Player.list = {};
+Player.onConnect = function(socket){
+
+	  var player = Player(socket.id);
+	  socket.on('keydown',function(data){
+		  
+		  switch(data.direction){
+			  case 'left': player.left = data.state;  break; 
+			  case 'up': player.up = data.state; break;
+			  case 'down': player.down = data.state; break;
+			  case 'right': player.right = data.state; break;
+		  }
+	  });
+}
+Player.onDisconnect = function(socket){
+     delete Player.list[socket.id];
+}
+Player.update = function(){
+ var pack_age = [];
+ for(var i in Player.list){
+ var player = Player.list[i]; 
+      player.update();
+      pack_age.push({
+
+          x:player.x,
+          y:player.y,
+          number: player.number
+});
+
+}
+ return pack_age;
+
 }
 var io = require('socket.io')(server,{});
 io.sockets.on('connection', function(socket){
@@ -35,56 +105,25 @@ io.sockets.on('connection', function(socket){
 	});
 	  // build user add him to list player and to socket
 	  socket.id = Math.random();
-	  
-	  var player = Player(socket.id)
-	  socket.on('keydown',function(data){
-		  
-		  switch(data.direction){
-			  case 'left': player.left = data.state; break;
-			  case 'up': player.up = data.state; break;
-			  case 'down': player.down = data.state; break;
-			  case 'right': player.right = data.state; break;
-		  }
-	  });
-	  
-	  LIST_PLAYER[socket.id] = player;
 	  LIST_SOCKET[socket.id] = socket;
-
+ 
+      Player.onConnect(socket);
 	  
 	// disconect user die socket
 socket.on('disconnect', function(){
-     delete LIST_PLAYER[socket.id];
-	 delete LIST_SOCKET[socket.id];
+     delete LIST_SOCKET[socket.id];
+     Player.onDisconnect(socket);
+  
 });
 
 });
 setInterval(function(){
-	var pack_age = [];
+            var pack_age =  Player.update();
 	
-	 for(var i in LIST_PLAYER){		
-	    var player = LIST_PLAYER[i];
-		
-		playerMovement(player);
-            		
-		 pack_age.push(player);
-	 }
 	 for(var i in LIST_SOCKET){
 		 var socket = LIST_SOCKET[i];
 		 socket.emit('movement',pack_age);
 	 }
 	 
 	
-},1000/30);
-function playerMovement(player){
-	
-	if(player.right)
-		player.x += player.speed;	        
-	if(player.left)
-		player.x -= player.speed;		
-	if(player.down)
-		player.y += player.speed;
-	if(player.up)
-		player.y -= player.speed;
-	
-	
-}
+},1000/25);
